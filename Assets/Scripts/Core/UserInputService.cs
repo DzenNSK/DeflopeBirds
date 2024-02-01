002038e79018660ed9ae47efca124d1dae9750ac
agaag <antgaag@gmail.com>
2024-02-02 05:00:18 +0700

    Touch fix

--- Assets/Scripts/Core/UserInputService.cs
+++ Assets/Scripts/Core/UserInputService.cs
@@ -24,7 +24,15 @@ namespace Dzen.DeflopeBirds
                 return;
             }
 
+            if(Input.touchCount > 0) 
+            {
+                touch = Input.GetTouch(0).phase == TouchPhase.Began;
+            }
+
+#if UNITY_STANDALONE_WIN
             touch = Input.anyKeyDown;
+#endif
+
         }
     }
 }
